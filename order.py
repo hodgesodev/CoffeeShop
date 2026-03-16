4182451ad6d631f8a0292ec621c3b8ed0c8a04b8
hodgesodev <hodgeso@proton.me>
2026-03-16 11:24:09 -0400

    Added status orders

--- order.py
+++ order.py
@@ -1,9 +1,18 @@
 from drink import Drink
+from enum import Enum
+
+class OrderStatus(Enum):
+    INITIALIZED = 0
+    PENDING = 1
+    IN_PROGRESS = 2
+    COMPLETED = 3
+    FAILED = -1
 
 class Order:
     def __init__(self):
         self._price: int = 0
         self._drinks: dict[Drink, int] = {} # Drink: Count
+        self._status: OrderStatus = OrderStatus.INITIALIZED
 
     def update_price(self):
         price = 0
@@ -31,6 +40,9 @@ class Order:
 
         self.update_price()
 
+    def update_status(self, status: OrderStatus):
+        self._status = status
+
     def get_price(self):
         price = self._price
         return price
@@ -38,3 +50,7 @@ class Order:
     def get_drinks(self):
         price = self._price
         return price
+
+    def get_status(self):
+        status = self._status
+        return status
